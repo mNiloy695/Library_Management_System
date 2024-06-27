cab406da54383b2220f73191f27159fd934c5509
mNiloy695 <mniloy695@gmail.com>
2024-06-27 20:03:32 +0600

    database added

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+        
     )
 }
 
